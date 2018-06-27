f644f0ee1a6e6121dc48289da079179f8040b163
Owen <karpfy16@gmail.com>
2018-06-27 18:56:11 -0400

    stuff to be added

---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='pynsta',
+    version='0.25',
+    description='A module for doing cool stuff with various websites, currently only operational for Instagram.',
+    url='https://github.com/HalfEmpty19/ArtiPy',
+    author='HalfEmpty',
+    packages=['pynsta',],
+    license='MIT',
+    author_email='halfemptynotfull@gmail.com',
+	long_description=open('README.txt').read(),
+
+)
